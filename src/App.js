1b4cf238aeb19aca36a44d776d549f9e58858e1c
tmcdonald98 <ThomasMcDonald1998@Gmail.com>
2020-09-16 14:12:30 -0700

    fixed wording and replaced image with gif

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import './index.css';
 import linkedIn from "./icons/linkedin.png"
 import gitHub from "./icons/github-image.png"
-import beerViz from "./Images/BeerViz.PNG"
+import beerViz from "./Images/BeerProduction.gif"
 import gitHubW from "./icons/github.png"
 import resume from "./icons/resume.png"
 import resumePDF from "./Documents/Resume (Thomas McDonald).pdf"
@@ -29,7 +29,7 @@ function App() {
           <div className="Body">
             Hi, I'm Thomas! I recently graduated from Gonzaga with my BS in computer science and 
             now I'm searching for a software engineering position. I have worked two summer internships 
-            and even I have seen some success in starting a company, Adventure Game Works. For this company, 
+            and even seen some success in starting a company, Adventure Game Works. For this company, 
             I prototyped a web application using ReactJS that allowed users to understand and customize 
             their purchased events. Adventure Game Works went on to place second in both the Northwest 
             Entrepreneurial Competition and the Dempsey Startup Competition. I took this experience and used 
@@ -39,7 +39,7 @@ function App() {
           </div>
           <div className="Body">
             I pick up on new technologies quickly and I’m eager to dive deep into this industry. I believe my greatest strengths are my ability to communicate with others and my creative thought process. 
-            Check out my socials, resume and past project.
+            Check out my socials, resume and past projects.
           </div>
           <div className="Icon-Container">
             <form className="Link-Wrapper" action="https://www.linkedin.com/in/thomasmcdonald1998/">
